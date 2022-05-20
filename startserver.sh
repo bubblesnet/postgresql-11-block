6067c13c18371e9e46f823243534ff9ad549ccc3
bubblesnet <73856491+bubblesnet@users.noreply.github.com>
2022-05-20 16:08:35 -0400

    delete all init_db functionality

--- startserver.sh
+++ startserver.sh
@@ -1,5 +1,11 @@
+echo "Copying configuration files into /etc/postgresql/11/main/"
+ls -l /postgresql_shared/conf
 cp /postgresql_shared/conf/* /etc/postgresql/11/main/
+echo "Restarting postgresql"
 /etc/init.d/postgresql restart
+echo "Changing postgres password to $POSTGRESQL_PASSWORD"
 sudo -u postgres psql -c "ALTER USER postgres PASSWORD '$POSTGRESQL_PASSWORD';"
+echo "Running migrations from /postgresql_shared/migrations"
+ls -l /postgresql_shared/migrations
 /migrate -source file://postgresql_shared/migrations -database postgres://postgres:postgres@localhost:5432/$POSTGRESQL_DBNAME?sslmode=disable up
 tail -F /var/log/postgresql/postgresql-11-main.log
