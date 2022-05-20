722aa0372c8a6884dbca065c927011bafc94909a
bubblesnet <73856491+bubblesnet@users.noreply.github.com>
2022-05-19 20:34:05 -0400

    init_db call

--- startserver.sh
+++ startserver.sh
@@ -2,12 +2,12 @@ cp /postgresql_shared/conf/* /etc/postgresql/11/main/
 /etc/init.d/postgresql restart
 if [ -f "/postgresql_shared/bin/init_db.sh" ]
 then
-  echo "File is found"
+  echo "/postgresql_shared/bin/init_db.sh file is found, running init"
   chmod +x /postgresql_shared/bin/init_db.sh
   /postgresql_shared/bin/init_db.sh
   rm /postgresql_shared/bin/init_db.sh
 else
-   echo "File is not found"
+   echo "/postgresql_shared/bin/init_db.sh file is not found, skipping init"
 fi
 
 /migrate -source file://postgresql_shared/migrations -database postgres://postgres:postgres@localhost:5432/$POSTGRESQL_DBNAME?sslmode=disable up
