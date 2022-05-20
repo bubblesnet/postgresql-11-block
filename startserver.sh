f32d083a8537b75e606e6614ff49b88c056cd7fa
bubblesnet <73856491+bubblesnet@users.noreply.github.com>
2022-05-20 11:47:08 -0400

    delete all init_db functionality

--- startserver.sh
+++ startserver.sh
@@ -1,14 +1,5 @@
 cp /postgresql_shared/conf/* /etc/postgresql/11/main/
 /etc/init.d/postgresql restart
-if [ -f "/postgresql_shared/bin/init_db.sh" ]
-then
-  echo "/postgresql_shared/bin/init_db.sh file is found, running init"
-  chmod +x /postgresql_shared/bin/init_db.sh
-  /postgresql_shared/bin/init_db.sh
-  rm /postgresql_shared/bin/init_db.sh
-else
-   echo "/postgresql_shared/bin/init_db.sh file is not found, skipping init"
-fi
-
+sudo -u postgres psql -c "ALTER USER postgres PASSWORD '$POSTGRESQL_PASSWORD';"
 /migrate -source file://postgresql_shared/migrations -database postgres://postgres:postgres@localhost:5432/$POSTGRESQL_DBNAME?sslmode=disable up
 tail -F /var/log/postgresql/postgresql-11-main.log
