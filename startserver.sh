6202510be40e337611bd7d0e6e47872623c9f4b6
bubblesnet <73856491+bubblesnet@users.noreply.github.com>
2022-05-20 17:04:43 -0400

    remove client-specific stuff

--- startserver.sh
+++ startserver.sh
@@ -1,11 +1,16 @@
+#!/bin/bash
 echo "Copying configuration files into /etc/postgresql/11/main/"
 ls -l /postgresql_shared/conf
 cp /postgresql_shared/conf/* /etc/postgresql/11/main/
 echo "Restarting postgresql"
 /etc/init.d/postgresql restart
+
 echo "Changing postgres password to $POSTGRESQL_PASSWORD"
+string=$POSTGRESQL_PASSWORD
+len=#{$string}
+if [ $len gt 0 ]; then
 sudo -u postgres psql -c "ALTER USER postgres PASSWORD '$POSTGRESQL_PASSWORD';"
-echo "Running migrations from /postgresql_shared/migrations"
-ls -l /postgresql_shared/migrations
-/migrate -source file://postgresql_shared/migrations -database postgres://postgres:postgres@localhost:5432/$POSTGRESQL_DBNAME?sslmode=disable up
+sudo psql -c "ALTER USER postgres PASSWORD '$POSTGRESQL_PASSWORD';"
+fi
+
 tail -F /var/log/postgresql/postgresql-11-main.log
